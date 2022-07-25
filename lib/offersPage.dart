0026129b2c9d7669b0073b075fa921b1033d37ba
jacobcwright <jacob.wright.ut@gmail.com>
2022-07-25 17:51:58 -0600

    multiple offers

--- lib/offersPage.dart
+++ lib/offersPage.dart
@@ -5,7 +5,14 @@ class OffersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Offer(title: "My greatest offer ever", description: "Buy 1, get 10 for free");
+    return Column(
+      children: const [
+        Offer(title: "My greatest offer ever", description: "Buy 1, get 10 for free"),
+        Offer(title: "My greatest offer ever", description: "Buy 1, get 10 for free"),
+        Offer(title: "My greatest offer ever", description: "Buy 1, get 10 for free"),
+        Offer(title: "My greatest offer ever", description: "Buy 1, get 10 for free"),
+      ],
+    );
   }
 }
 
