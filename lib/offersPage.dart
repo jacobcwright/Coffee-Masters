efa1961ef45935f8cee8c5ecbba74a62a87f75aa
jacobcwright <jacob.wright.ut@gmail.com>
2022-07-25 17:52:24 -0600

    listview = scrollable column

--- lib/offersPage.dart
+++ lib/offersPage.dart
@@ -5,7 +5,7 @@ class OffersPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return ListView(
       children: const [
         Offer(title: "My greatest offer ever", description: "Buy 1, get 10 for free"),
         Offer(title: "My greatest offer ever", description: "Buy 1, get 10 for free"),
