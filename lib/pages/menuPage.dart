70a2fd9d89cce1732a6edda561e531800cf0d75f
jacobcwright <jacob.wright.ut@gmail.com>
2022-07-25 19:29:33 -0600

    added add button on product page

--- lib/pages/menuPage.dart
+++ lib/pages/menuPage.dart
@@ -7,32 +7,68 @@ class MenuPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var p = Product(1, "Dummy Product", 1.25, "");
-    return ProductItem(product: p,);
+    var p2 = Product(2, "Dummy Product 2", 2.99, "");
+
+    return Column(
+      children: [
+        ProductItem(
+          product: p,
+          onAdd: () {},
+        ),
+        ProductItem(
+          product: p2,
+          onAdd: () {},
+        ),
+      ],
+    );
   }
 }
 
 class ProductItem extends StatelessWidget {
   final Product product;
+  final Function onAdd;
 
-  const ProductItem({Key? key, required this.product}) : super(key: key);
+  const ProductItem({Key? key, required this.product, required this.onAdd})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(8),
+      padding: const EdgeInsets.all(8),
       child: Card(
         elevation: 4,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Image.asset("images/black_coffee.png"),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(product.name, style: TextStyle(fontWeight: FontWeight.bold),),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("\$${product.price}"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        product.name,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("\$${product.price}"),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: ElevatedButton(
+                      onPressed: () {
+                        onAdd(product);
+                      },
+                      child: const Text("Add")),
+                )
+              ],
             ),
           ],
         ),
