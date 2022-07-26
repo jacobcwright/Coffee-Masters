57120860e909a3d92412cce1b84b47f795fa2974
jacobcwright <jacob.wright.ut@gmail.com>
2022-07-25 19:14:12 -0600

    added product to menu

--- lib/pages/menuPage.dart
+++ lib/pages/menuPage.dart
@@ -1,3 +1,4 @@
+import 'package:coffee_masters/dataModel.dart';
 import 'package:flutter/material.dart';
 
 class MenuPage extends StatelessWidget {
@@ -5,6 +6,37 @@ class MenuPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var p = Product(1, "Dummy Product", 1.25, "");
+    return ProductItem(product: p,);
+  }
+}
+
+class ProductItem extends StatelessWidget {
+  final Product product;
+
+  const ProductItem({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: Card(
+        elevation: 4,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset("images/black_coffee.png"),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(product.name, style: TextStyle(fontWeight: FontWeight.bold),),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("\$${product.price}"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
