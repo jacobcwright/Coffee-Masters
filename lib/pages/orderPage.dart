027dfc4f3f3e797a7ab4ddd946e10ea269f571aa
jacobcwright <jacob.wright.ut@gmail.com>
2022-07-29 15:18:41 -0600

    added orderItem

--- lib/pages/orderPage.dart
+++ lib/pages/orderPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../dataManager.dart';
+import '../dataModel.dart';
 
 class OrderPage extends StatelessWidget {
   final DataManager dataManager;
@@ -8,6 +9,52 @@ class OrderPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Text("Cart length: ${dataManager.cart.length}");
+  }
+}
+
+class OrderItem extends StatelessWidget {
+  final ItemInCart item;
+  final Function onRemove;
+  const OrderItem({Key? key, required this.item, required this.onRemove})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 8.0),
+                  child: Text("${item.quantity}x"),
+                )),
+            Expanded(
+                flex: 6,
+                child: Text(
+                  item.product.name,
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                )),
+            Expanded(
+                flex: 2,
+                child: Text("\$" +
+                    (item.product.price * item.quantity).toStringAsFixed(2))),
+            Expanded(
+                flex: 1,
+                child: IconButton(
+                    color: Theme.of(context).primaryColor,
+                    onPressed: () {
+                      onRemove(item.product);
+                    },
+                    icon: const Icon(Icons.delete)))
+          ],
+        ),
+      ),
+    );
   }
 }
