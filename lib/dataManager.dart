b7fc1b29aac231224fafeebd976c76293cad76a5
jacobcwright <jacob.wright.ut@gmail.com>
2022-07-28 20:32:12 -0600

    added data factories, json parsers, and request

--- lib/dataManager.dart
+++ lib/dataManager.dart
@@ -1,8 +1,31 @@
+import 'dart:convert';
+
 import 'package:coffee_masters/dataModel.dart';
+import 'package:http/http.dart' as http;
 
 class DataManager {
   List<Category>? _menu;
   List<ItemInCart> cart = [];
+
+  fetchMenu() async {
+    const url = "https://firtman.github.io/coffeemasters/api/menu.json";
+    var response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      var body = response.body as dynamic;
+      var decodeData = JsonDecoder(body) as List<dynamic>;
+      for (var json in decodeData) {
+        _menu?.add(Category.fromJson(json));
+      }
+    }
+  }
+
+  Future<List<Category>> getMenu() async {
+    if (_menu == null) {
+      await fetchMenu();
+    }
+    return _menu!;
+  }
 
   cartAdd(Product p) {
     bool found = false;
